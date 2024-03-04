92084a815a199efa5564193f69e89a89f227d6af
Brian <turtle40965@gmail.com>
2024-03-04 13:19:50 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// document.createElement
+
+const main = document.getElementById("main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent= "Brian is the champion"
